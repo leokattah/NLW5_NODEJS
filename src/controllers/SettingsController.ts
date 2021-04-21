4eeec06322ba85837fa61943e4b923d70ba020f3
leokattah <leokattah@gmail.com>
2021-04-21 16:56:23 -0300

    day 3 started

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,22 +1,21 @@
-import { Request, Response } from 'express';
-
-import { getCustomRepository } from 'typeorm';
-import { SettingsRepository } from '../repositories/SettingsRepository';
+import { Request, response, Response } from 'express';
+import { SettingsService } from '../services/SettingsService';
 
 class SettingsController {
   async create(req: Request, res: Response) {
     const { chat, username } = req.body;
 
-    const settingsRepository = getCustomRepository(SettingsRepository);
+    const settingsService = new SettingsService();
 
-    const setting = settingsRepository.create({
-      chat,
-      username,
-    });
+  try{
+    const settings = await settingsService.create({ chat, username });
 
-    await settingsRepository.save(setting);
-
-    return res.json(setting);
+    return res.json(settings);
+  }catch(err) {
+    return res.status(400).json({
+      message: err.message,
+    })
+  }
   }
 }
 
